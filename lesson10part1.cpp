acfe429e6db6bd9ff426da97e5d907612cccac84
Matthew Vu <MSV180000@utdallas.edu>
2019-11-24 23:51:32 -0600

    Did the 2D Array part 2 assignment
    Tranverse 2d array and read in infomation

--- lesson10part1.cpp
+++ lesson10part1.cpp
@@ -1,3 +1,9 @@
+/*
+Program will use two parallel arrays and read in student answers and correct answer into
+both respectively. After checking they are valid, the display function will output if there are any
+incorrect answers and see if the student passed or failed.
+*/
+
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
@@ -6,84 +12,94 @@
 
 using namespace std;
 // reads in array and file name
-int readAnswers(char read[], string fileName)  {
-     ifstream finput;
-     int totalFile = 0;
-     char answer;
-     finput.open(fileName.c_str());
-     if(!finput) {
+int readAnswers(char read[], string fileName)
+{
+    ifstream finput;
+    int totalFile = 0; // number of answers within a file
+    char answer;
+    finput.open(fileName.c_str());
+    if(!finput) // if file is not opened.
+    {
         return -1;
-     }
-     while(finput >> answer) {
+    }
+    while(finput >> answer) // checks to see when file is done being read in and loops accordingly.
+    {
         totalFile++;
-        if(totalFile > 30)
+        if(totalFile > 30) // if num answer is more than 30 set it to 30
             totalFile = 30;
-     }
-     finput.close();
-     finput.open(fileName.c_str());
-     for(int i = 0; i < totalFile; i++) {
-        finput >> read[i];
-     }
-     finput.close();
+    }
+    finput.close(); // closes the file
+    finput.open(fileName.c_str()); // open the file again and does how many number of answers are within the file
+    for(int i = 0; i < totalFile; i++)
+    {
+        finput >> read[i]; // reads input file contents into the given array
+    }
+    finput.close(); // closes
     return totalFile;
 }
-
-void displayResults(char student[], char answers[], int studentAnswer) {
+// display function is given the student answers and correct answer also, the number of answers within both files
+void displayResults(char student[], char answers[], int numAnswer)
+{
     double missed = 0, total = 0;
-    for(int i = 0; i < studentAnswer ; i++) {
-        if(student[i] != answers[i]) {
+    for(int i = 0; i < numAnswer ; i++)
+    {
+        if(student[i] != answers[i]) // if the one part of the array doesn't match the other array, output that it's a wrong answer
+        {
             cout << "Question " << i+1 << " has incorrect answer '" << student[i] << "', the correct answer is '" << answers[i] << "'\n";
             missed++;
         }
         total++;
     }
-    cout << missed << " questions were missed out of " << total << endl;
-    double grade = ((total - missed) / total) * 100;
+    cout << missed << " questions were missed out of " << total << endl; // shows questions missed out of total
+    double grade = ((total - missed) / total) * 100; // calc and output student grade
     cout << fixed << setprecision(1) << "The student grade is " << grade << "%" << endl;
-    if(grade >= 70) {
+    if(grade >= 70) // student passes or fails
+    {
         cout << "The student passed" << endl;
     }
-    else {
+    else
+    {
         cout << "The student failed" << endl;
     }
 }
-
+// main function
 int main()
 {
-    char student[30], answers[30];
-    int correctAnswer, studentAnswer;
+    char student[30], answers[30]; // two parallel arrays
+    int correctAnswerTotal, studentAnswerTotal; // vars that count the num answer in each file
 
     string inputFileName;
 
-    cout << "Enter student answers file name\n";
+    cout << "Enter student answers file name\n"; // reads file name from user
     cin >> inputFileName;
-    studentAnswer = readAnswers(student, inputFileName);
-    if(studentAnswer == -1) {
-         cout << "File \"" << inputFileName << "\" could not be opened\n";
-         exit(1);
+    studentAnswerTotal = readAnswers(student, inputFileName); // calls read function to get num answer from file
+    if(studentAnswerTotal == -1) // checks to see if file opens
+    {
+        cout << "File \"" << inputFileName << "\" could not be opened\n";
+        exit(1);
     }
-    else {
-        cout << "Enter correct answer file name\n";
+    else
+    {
+        cout << "Enter correct answer file name\n"; // // calls read function to get num answer from file
         cin >> inputFileName;
-        correctAnswer = readAnswers(answers, inputFileName);
-        if(correctAnswer == -1) {
+        correctAnswerTotal = readAnswers(answers, inputFileName);
+        if(correctAnswerTotal == -1) // validation check for the file not opening
+        {
             cout << "File \"" << inputFileName << "\" could not be opened\n";
-            exit(1);
+            exit(1); // exits program
         }
-        if(correctAnswer != studentAnswer) {
-            cout << "The student answers file has " << studentAnswer << " entries and the correct answers file has " << correctAnswer << " entries\n";
+        if(correctAnswerTotal != studentAnswerTotal) // checks to see if num answers in both files matches
+        {
+            cout << "The student answers file has " << studentAnswerTotal << " entries and the correct answers file has " << correctAnswerTotal << " entries\n";
             cout << "Grading cannot be done if they are not the same\n";
             exit(1);
         }
-        if(correctAnswer == 0 && studentAnswer == 0) {
+        if(correctAnswerTotal == 0 && studentAnswerTotal == 0) // makes sure if both files are not empty
+        {
             cout << "The number of student answers and correct answers are both 0\n";
             cout << "No grade can be calculated\n";
             exit(1);
         }
-        displayResults(student, answers, studentAnswer);
-
+        displayResults(student, answers, studentAnswerTotal); // calls the display function
     }
-
-
-
 }
---
+++ lesson10part2.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+const int MAX_COLUMNS = 5; // max columns in array
+
+// read input file and populate the array
+int readFile(double values[][MAX_COLUMNS], int maxRows, string inputFileName)
+{
+    ifstream finput; // stream declaration
+    int numRows = 0; // var that counts rows to be returned
+    finput.open(inputFileName.c_str()); //  // opens file
+    if(!finput) // if file not open return -1
+    {
+        return -1;
+    }
+    for(int j = 0; j < maxRows; j++) // traverses row and columns of array
+    {
+        for(int i = 0; i < MAX_COLUMNS; i++)
+        {
+            if(finput.eof()) // closes file and returns row after file is done being read in
+            {
+                finput.close();
+                return numRows;
+            }
+            else
+            {
+                finput >> values[j][i]; // reads in file content into array
+            }
+        }
+        numRows++; // increment row num
+    }
+    finput.close(); // closes in the case of edge case
+    return numRows;
+}
+// for the average in array
+double average(double values[][MAX_COLUMNS], int numberRows)
+{
+    double sum = 0;
+    double total = MAX_COLUMNS * numberRows;
+    for(int j = 0; j < numberRows; j++)
+    {
+        for(int i = 0; i < MAX_COLUMNS; i++)
+        {
+            sum += values[j][i];
+        }
+    }
+    double average = sum / total;
+    return average;
+}
+// average for a specified column (column)
+double columnAverage(double values[][MAX_COLUMNS], int column, int numberRows)
+{
+    double sum = 0, average;
+    for(int i = 0; i < numberRows; i++) // traverses the y axis of 2d array and adds it to sum
+    {
+        sum += values[i][column];
+    }
+    average = sum / numberRows; // gets average by dividing by number of rows
+    return average;
+}
+// find smallest val in row
+double smallestRowValue(double values[][MAX_COLUMNS], int row)
+{
+    double small = values[row][0]; // sets the first value in row to the smallest
+    for(int i = 0; i < MAX_COLUMNS; i++) // traverses x axis of 2d array
+    {
+        if(small > values[row][i]) // checks if the next column has lower value
+        {
+            small = values[row][i]; // sets smallest value equal to current position.
+        }
+    }
+    return small;
+}
+
+int main()
+{
+    const int MAX_ROWS = 30; // const for num of rows
+    string inputFileName;
+    double grades[MAX_ROWS][MAX_COLUMNS]; // declares 2D array//
+
+    cout << "Enter input file name\n"; // prompt user for file name
+    cin >> inputFileName;
+
+    int actualRows = readFile(grades, MAX_ROWS, inputFileName);
+    if(actualRows == -1) // if file does not open exit and display error message
+    {
+        cout << "File \"" << inputFileName << "\" could not be opened\n";
+        exit(1);
+    }
+    if(actualRows == 0) // no rows means no content in file, which means you exit
+    {
+        cout << "The input file \"" << inputFileName << "\" did not contain any data\n";
+    }
+    else
+    {
+        double averageVal = average(grades, actualRows); // calls average value function of all contents in file
+        cout << "Processing " << actualRows << " rows, and 5 columns\n";
+        cout << fixed << setprecision(2) << "Average for all values is " << averageVal << endl;
+
+        for(int i = 0; i < MAX_COLUMNS; i++) // repeats max columns number of times
+        {
+            cout << "Average for column " << i << " is " << columnAverage(grades, i, actualRows)<< endl; // calls column average function
+        }
+        for(int j = 0; j < actualRows; j++) // repeats actual number of time
+        {
+            cout << "Smallest value for row " << j << " is " << smallestRowValue(grades, j) << endl; // call small val row function
+        }
+    }
+}
